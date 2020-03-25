b23cd9613b23dcaaea25cf416db45e17f2f9b21f
Eric Kartali <erkartali@gmail.com>
2020-03-25 12:25:31 -0500

    added start timer function and hid game questions till the timer starts

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,9 +3,14 @@ var radios = [];
 var checked = [];
 var correct = 0;
 var incorrect = 0;
+var time = 60;
+var clicked = false;
+
+
+
 
 var  collectAnswers = () => {
-
+    
     // get all radio buttons
     for (let i = 0; i < inputs.length; i++) {
         if (inputs[i].type === 'radio') {
@@ -13,30 +18,55 @@ var  collectAnswers = () => {
         }
     }
     console.log(radios)
-
+    
     // loop through all radio buttons and create an array for only checked buttons
     for (let j = 0; j < radios.length; j++) {
-         if (radios[j].checked === true) {
-             checked.push(radios[j]);
-         }
-     }
+        if (radios[j].checked === true) {
+            checked.push(radios[j]);
+        }
+    }
     console.log(checked);
-
+    
     // loop through all checked button and increment correct and incorrect
     for (let k = 0; k < checked.length; k++) {
-         if (checked[k].value === 'correct') {
-             correct++;
-         } else if (checked[k].value === 'incorrect') {
-             incorrect++;
-         }  
-        }
-        console.log('correct:', correct);
-        console.log('incorrect:', incorrect);
-        $('#correct-answers').html(correct);
-        $('#incorrect-answers').html(incorrect);
+        if (checked[k].value === 'correct') {
+            correct++;
+        } else if (checked[k].value === 'incorrect') {
+            incorrect++;
+        }  
+    }
+    console.log('correct:', correct);
+    console.log('incorrect:', incorrect);
+    
+
+    if (!clicked) {
+        $('#correct-answers').html('You got ' + correct + ' right!');
+        $('#incorrect-answers').html('You got ' + incorrect + ' wrong!');
+        clicked = true;
+    } 
+    
+    window.clearInterval(startTheTimer);
 }
 
+var timer = () => {
+    time--;
+    $('#timer').html(time);
+}
+
+var startTheTimer = () => {
+    setInterval(timer, 1000);
+    $('.game-questions').css('display', 'initial');
+}
+
+$('#countdown').click(startTheTimer);
 $('.radio-submit').click(collectAnswers);
+var time2 = 60;
 
+var timer2 = () => {
+    time2--;
+    $('.time2').html(time2);
+}
 
+$('.begin').click(setInterval(timer2, 1000));
+$('.end').click(clearInterval(timer2));
 
